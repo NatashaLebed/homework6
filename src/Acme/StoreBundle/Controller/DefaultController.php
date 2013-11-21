7dc4d3aae282962eabf4402da5ed68e5cb89a1f6
NatashaLebed <Natasha.lebed@gmail.com>
2013-11-21 15:01:59 +0200

    FixtureBundle

--- src/Acme/StoreBundle/Controller/DefaultController.php
+++ src/Acme/StoreBundle/Controller/DefaultController.php
@@ -7,6 +7,9 @@ use Acme\StoreBundle\Entity\Product;
 use Acme\StoreBundle\Entity\Category;
 use Acme\StoreBundle\Entity\ProductRepository;
 use Symfony\Component\HttpFoundation\Response;
+use Doctrine\Common\DataFixtures\Loader;
+use Doctrine\Common\DataFixtures\Executor\ORMExecutor;
+use Doctrine\Common\DataFixtures\Purger\ORMPurger;
 
 class DefaultController extends Controller
 {
@@ -81,95 +84,25 @@ class DefaultController extends Controller
         ));
     }
 
-
-
-//======================test read/write database  ==================
-
-    public function createAction()
+    public function fixtureAction()
     {
-        $category = new Category();
-        $category->setName('Children');
-        $category->setNamecat("Kid's dress, shoes");
-
-
-        $product = new Product();
-        $product->setName('dress');
-        $product->setPrice(10);
-        $product->setDescription('super puper dress for kids');
-        // relate this product to the category
-        $product->setCategory($category);
-
-        $em = $this->getDoctrine()->getManager();
-        $em->persist($category);
-        $em->persist($product);
-        $em->flush();
-
-        return new Response(
-            'Created product id: '.$product->getId().' and category id: '.$category->getId()
-        );
+        return $this->render('AcmeStoreBundle:Default:fixture.html.twig');
     }
 
-    public function showAction($id)
+    public function fixtureDoneAction()
     {
-        $product = $this->getDoctrine()
-            ->getRepository('AcmeStoreBundle:Product')
-            ->find($id);
+            $em = $this->getDoctrine()->getManager();
+            $loader = new Loader();
+            $loader->loadFromDirectory(__DIR__ . '/../DataFixtures');
 
-        if (!$product) {
-            throw $this->createNotFoundException(
-                'No product found for id '.c
-            );
-        }
-        $categoryName = $product->getCategory()->getName();
+            $purger = new ORMPurger();
+            $executor = new ORMExecutor($em, $purger);
+            $executor->execute($loader->getFixtures());
 
-        return new Response('product name '.$product->getName().'category name '.$categoryName);
+        return $this->render('AcmeStoreBundle:Default:fixture_done.html.twig');
     }
 
-    public function updateAction($id)
-    {
-        $em = $this->getDoctrine()->getManager();
-        $product = $em->getRepository('AcmeStoreBundle:Product')->find($id);
 
-        if (!$product) {
-            throw $this->createNotFoundException(
-                'No product found for id '.$id
-            );
-        }
-
-        $product->setName('New product name!');
-        $em->flush();
-
-        return new Response('product name '.$product->getName());
-    }
-
-    public function selectAction()
-    {
-        $em = $this->getDoctrine()->getManager();
-        $query = $em->createQuery(
-            'SELECT p
-            FROM AcmeStoreBundle:Product p
-            WHERE p.price > :price
-            ORDER BY p.price ASC'
-        )->setParameter('price', '19.99');
-        $query->setMaxResults(1);
-
-        try {
-            $product = $query->getSingleResult();
-        } catch (\Doctrine\Orm\NoResultException $e) {
-            $product = null;
-        }
-
-       return new Response('product name '.$product->getName());
-    }
-
-    public function findAllAction()
-    {
-        $em = $this->getDoctrine()->getManager();
-        $product = $em->getRepository('AcmeStoreBundle:Product')
-            ->findByNameAllOrdered();
-        //$str = vardump($product,true);
-        return new Response('product name sdfsfs' );
-    }
 }
 
 
